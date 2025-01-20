22b192716b6554c5657fa25416930da29b408fa7
SleepyWoodpecker <shawnweichew@gmail.com>
2025-01-20 11:34:26 -0800

    bugfix: CSS was not loading for multi-play button, so it is not hardcoded

--- open-game.js
+++ open-game.js
@@ -28,8 +28,46 @@ function addPlayButton(gameFrameUrl) {
       .querySelector("#button-play")
       .cloneNode(true);
     multiplayerButton.id = "multiplayer-button";
-    multiplayerButton.classList.remove("play-button-disabled");
-    multiplayerButton.style.backgroundColor = "#8e44e6";
+    multiplayerButton.style.cssText = `
+    -webkit-box-align: center;
+    -moz-box-align: center;
+    -webkit-box-pack: center;
+    -moz-box-pack: center;
+    -webkit-align-items: center;
+    align-items: center;
+    -webkit-animation: scale-easeOutElastic-reversed .6s;
+    -moz-animation: scale-easeOutElastic-reversed .6s;
+    animation: scale-easeOutElastic-reversed .6s;
+    background-color: #8e44e6;
+    border-radius: 25px;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    color: #fff;
+    cursor: pointer;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -moz-box;
+    display: flex;
+    font-size: 1.5em;
+    font-weight: 500;
+    height: 50px;
+    -webkit-justify-content: center;
+    justify-content: center;
+    letter-spacing: 1px;
+    min-width: 130px;
+    padding: 5px 15px;
+    text-decoration: none;
+    -webkit-transition: -webkit-transform .15s ease;
+    transition: -webkit-transform .15s ease;
+    -moz-transition: transform .15s ease,-moz-transform .15s ease;
+    transition: transform .15s ease;
+    transition: transform .15s ease,-webkit-transform .15s ease,-moz-transform .15s ease;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    `;
     multiplayerButton.style.margin = "auto";
 
     // change the child elements
@@ -41,8 +79,6 @@ function addPlayButton(gameFrameUrl) {
     const buttonIcon = multiplayerButton.querySelector(".icon");
     if (buttonIcon) {
       multiplayerButton.removeChild(buttonIcon);
-      // TODO @Shawn: add an icon
-      // buttonIcon.style.backgroundImage = "url(./icons/multiplayer.svg)";
     }
 
     playButtonContainer.appendChild(multiplayerButton);
